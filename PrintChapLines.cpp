e7fcd82bd41d44164628d00156972eebec836615
Srivastava Dhruv <h3566779@academy11.cs.hku.hk>
2020-04-19 23:19:23 +0800

    Editted PrintChapLines function to print in desired format

--- PrintChapLines.cpp
+++ PrintChapLines.cpp
@@ -7,10 +7,22 @@ using namespace std;
 void PrintChapLines(string *FileName, int *Lines){
   ifstream fin((*FileName).c_str());
   string *f = new string("");
+  string *s = new string("");
   while(*Lines > 0){
     getline(fin,*f);
     cout<<*f<<endl;
     *Lines = *Lines - 1;
+    cout<<endl;
+    while(true){
+      cout<<"Enter s to continue"<<endl;;
+      cin>>*s;
+      if((*s) == "s"){
+        break;
+      }
+      else{
+        cout<<"Enter s to continue"<<endl;
+      }
+    }
   }
   fin.close();
   delete Lines;
@@ -21,7 +33,7 @@ void PrintChapLines(string *FileName, int *Lines){
 
 int main(){
   string *FN = new string("Chapter1.txt");
-  int *L = new int(1);
+  int *L = new int(10);
   PrintChapLines(FN, L);
   delete FN;
   FN = 0;
